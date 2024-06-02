74672cf37725641d6cd5944fb016dc907ee06a2a
sam2800ml <sam2800ml@gmail.com>
2024-06-02 17:43:07 -0500

    yolo

--- Proyecto_final/arUco.py
+++ Proyecto_final/arUco.py
@@ -1,5 +1,18 @@
 import cv2
 import cv2.aruco as aruco
+import numpy as np
+
+def aruco_caption(frame):
+    # Obtiene el diccionario de marcadores ArUco
+    aruco_dict = aruco.Dictionary_get(aruco.DICT_4X4_1000)
+    # Configura los parámetros del detector
+    parameters =  aruco.DetectorParameters_create()
+    # Detecta los marcadores ArUco en la imagen
+    corners, _, _ = aruco.detectMarkers(gray, aruco_dict, parameters=parameters)
+    print(corners)
+    
+    return corners
+
 
 
 cap = cv2.VideoCapture(0)
@@ -11,20 +24,33 @@ while True:
     # Convierte el frame a escala de grises
     gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
     
-    # Obtiene el diccionario de marcadores ArUco
-    aruco_dict = aruco.Dictionary_get(aruco.DICT_4X4_1000)
+    corners = aruco_caption(gray)
     
-    # Configura los parámetros del detector
-    parameters =  aruco.DetectorParameters_create()
+    if len(corners) > 0:
+        for marker_corners in corners:
+            marker_corners = marker_corners[0].astype(np.float32) 
+            aruco_perimeter = cv2.arcLength(marker_corners,True)
+            side_length_cm = 10
+            perimeter_cm = 4 * side_length_cm  
+            pixel_cm_ratio = aruco_perimeter / perimeter_cm
+            rect = cv2.minAreaRect(marker_corners)
+            (x, y), (w, h), angle = rect
+
+            object_width = w / pixel_cm_ratio
+            object_height = h / pixel_cm_ratio
     
-    # Detecta los marcadores ArUco en la imagen
-    corners, ids, rejectedImgPoints = aruco.detectMarkers(gray, aruco_dict, parameters=parameters)
+            cv2.putText(frame, "Width {} cm".format(round(object_width, 1)), (int(x - 100), int(y - 20)), cv2.FONT_HERSHEY_PLAIN, 2, (100, 200, 0), 2)
+            cv2.putText(frame, "Height {} cm".format(round(object_height, 1)), (int(x - 100), int(y + 15)), cv2.FONT_HERSHEY_PLAIN, 2, (100, 200, 0), 2)
+        
+        # Dibuja los marcadores detectados en el frame
+        frame = aruco.drawDetectedMarkers(frame, corners)
+    
     
     # Dibuja los marcadores detectados en el frame
     frame_markers = aruco.drawDetectedMarkers(frame, corners)
     
     # Muestra el frame con los marcadores
-    cv2.imshow('frame', frame_markers)
+    cv2.imshow('frame', frame)
     
     # Espera a que se presione la tecla 'q' para salir del bucle
     if cv2.waitKey(1) & 0xFF == ord('q'):
--- Yolov8/yolov8.py
+++ Yolov8/yolov8.py
@@ -1,49 +1,78 @@
+from ultralytics import YOLO
 import cv2
 import numpy as np
+import matplotlib.pyplot as plt
+import cv2.aruco as aruco
 
-# Load the ONNX model
-net = cv2.dnn.readNetFromONNX("best (1).onnx")
 
-# Initialize the webcam
+model = YOLO("Yolov8/best.pt")
+
 cap = cv2.VideoCapture(0)
+
+def aruco_caption(frame):
+    aruco_dict = aruco.Dictionary_get(aruco.DICT_4X4_1000)
+    parameters =  aruco.DetectorParameters_create()
+    corners, _, _ = aruco.detectMarkers(frame, aruco_dict, parameters=parameters)
+    pixel_cm_ratio = None
+    if len(corners) > 0:
+        for marker_corners in corners:
+            marker_corners = marker_corners[0].astype(np.float32) 
+            aruco_perimeter = cv2.arcLength(marker_corners,True)
+            side_length_cm = 10
+            perimeter_cm = 4 * side_length_cm  
+            pixel_cm_ratio = aruco_perimeter / perimeter_cm
+            rect = cv2.minAreaRect(marker_corners)
+            (x, y), (w, h), angle = rect
+          
+            object_width = w / pixel_cm_ratio
+            object_height = h / pixel_cm_ratio
+    
+            cv2.putText(frame, "Width {} cm".format(round(object_width, 1)), (int(x - 100), int(y - 20)), cv2.FONT_HERSHEY_PLAIN, 2, (100, 200, 0), 2)
+            cv2.putText(frame, "Height {} cm".format(round(object_height, 1)), (int(x - 100), int(y + 15)), cv2.FONT_HERSHEY_PLAIN, 2, (100, 200, 0), 2)
+
+    frame_markers = aruco.drawDetectedMarkers(frame, corners)
+    return frame_markers, pixel_cm_ratio
+
+def detection_model(frame,results,pixel_cm_ratio ,threshold=0.7):
+    for i, result in enumerate(results.boxes.data.tolist()):
+        x1, y1, x2, y2, score, class_id = result
+        print(score)
+        if score > threshold:
+            x1,y1,x2,y2 = int(x1), int(y1), int(x2), int(y2)
+            print(f"Coordenadas:{ x1,y1,x2,y2}")
+            print(f"Confianza {score}")
+            print(f" Id: {class_id}")
+            if pixel_cm_ratio:
+
+                w = x2-x1
+                h = y2-y1
+                object_width = w / pixel_cm_ratio
+                object_height = h / pixel_cm_ratio
+                cv2.putText(frame, "Width {} cm".format(round(object_width, 1)), (int(x1 - 100), int(y1 - 20)), cv2.FONT_HERSHEY_PLAIN, 2, (100, 200, 0), 2)
+                cv2.putText(frame, "Height {} cm".format(round(object_height, 1)), (int(x1 - 100), int(y1 + 15)), cv2.FONT_HERSHEY_PLAIN, 2, (100, 200, 0), 2)
+
+            cv2.rectangle(frame,(x1,y1),(x2,y2), (0,0,0),1)
+            cv2.putText(frame, str(class_id), (x1,y1 -10), cv2.FONT_HERSHEY_COMPLEX, 1, (0,255,0), cv2.LINE_AA)
+            
+
+
+            return frame
+        return frame
+    return frame
+    
+
 
 while True:
     ret, frame = cap.read()
-    if not ret:
-        break
-
-    # Prepare the frame for object detection
-    input_image = cv2.resize(frame, (640, 640))  # Resize to the size your model expects
-    blob = cv2.dnn.blobFromImage(input_image, scalefactor=1/255.0, size=(640, 640), swapRB=True, crop=False)
-    net.setInput(blob)
-
-    # Run the model
-    detections = net.forward()
-
-    # Process the detections
-    for detection in detections[0]:
-        confidence = detection[4]  # Assuming confidence is at index 4
-        if confidence > 0.5:  # Confidence threshold
-            class_id = int(detection[5])
-            x_center = int(detection[0] * frame.shape[1])
-            y_center = int(detection[1] * frame.shape[0])
-            width = int(detection[2] * frame.shape[1])
-            height = int(detection[3] * frame.shape[0])
-            x_left = x_center - width // 2
-            y_top = y_center - height // 2
-
-            # Draw a bounding box
-            cv2.rectangle(frame, (x_left, y_top), (x_left + width, y_top + height), (0, 255, 0), 2)
-            label = f"Class {class_id}: {confidence:.2f}"
-            cv2.putText(frame, label, (x_left, y_top - 10), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0, 255, 0), 2)
-
-    # Display the frame with detections
-    cv2.imshow("Object Detection", frame)
-
-    # Break the loop on 'q' key press
+    results = model.predict(frame)[0]
+    frame, pixel_cm_ratio = aruco_caption(frame)
+    frame = detection_model(frame,results, pixel_cm_ratio)
+    cv2.imshow('frame', frame)
+    
+    # Espera a que se presione la tecla 'q' para salir del bucle
     if cv2.waitKey(1) & 0xFF == ord('q'):
         break
 
-# Release the webcam and close windows
+# Libera los recursos
 cap.release()
 cv2.destroyAllWindows()
